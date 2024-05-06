4eacae82a6715c8036668ebb5eded4de395201d0
jaeYeon <A23670@nis.co.kr>
2024-05-07 08:26:02 +0900

    [FIX] userId, userName, password @NotNull 적용

--- src/main/java/com/example/boards/dto/BoardForm.java
+++ src/main/java/com/example/boards/dto/BoardForm.java
@@ -1,13 +1,18 @@
 package com.example.boards.dto;
 
+import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
 public class BoardForm {
+    //@NotNull - null 허용 x, @NotBlank - string 타입 빈 값 허용 x, @NotEmpty - null, 빈 문자열 허용x
+    @NotNull
     private String userId;
+    @NotNull
     private String userName;
+    @NotNull
     private String password;
     private String title;
     private String content;
