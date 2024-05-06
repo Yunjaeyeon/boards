e916227dbd4c8410c1792e14fe1b3ee9792e9b08
jaeYeon <A23670@nis.co.kr>
2024-05-07 08:36:49 +0900

    [FIX] content, title @Size적용

--- src/main/java/com/example/boards/dto/BoardForm.java
+++ src/main/java/com/example/boards/dto/BoardForm.java
@@ -1,22 +1,24 @@
 package com.example.boards.dto;
 
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotEmpty;
-import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.*;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
 public class BoardForm {
-    //@NotNull - null 허용 x, @NotBlank - string 타입 빈 값 허용 x, @NotEmpty - null, 빈 문자열 허용x
+    //@NotNull - null x
+    //@NotEmpty - null,("") x
+    //@NotBlank - (""), (" "), null x
     @NotBlank
     private String userId;
     @NotBlank
     private String userName;
     @NotBlank
     private String password;
+    @Size(max = 200)
     private String title;
+    @Size(max = 1000)
     private String content;
     private String email;
     private String phoneNo;
