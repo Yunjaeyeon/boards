bfdfacd76f5d6bb9868e975577d28ff36b8ccab6
jaeYeon <A23670@nis.co.kr>
2024-05-07 09:18:04 +0900

    [FIX] password @Pattern 적용, email @Email 적용

--- src/main/java/com/example/boards/dto/BoardForm.java
+++ src/main/java/com/example/boards/dto/BoardForm.java
@@ -14,12 +14,15 @@ public class BoardForm {
     private String userId;
     @NotBlank
     private String userName;
+    // 비밀번호는 대소문자로 이뤄져야하고 숫자가 5개이상 특수문자가 *!@#$%을 포함을 2개이상 해야한다.
     @NotBlank
+    @Pattern(regexp = "^(?=.*[a-zA-Z])(?=(?:.*[0-9]){5,})(?=(?:.*[!@#$%]){2,}).*$")
     private String password;
     @Size(max = 200)
     private String title;
     @Size(max = 1000)
     private String content;
+    @Email
     private String email;
     private String phoneNo;
 }
